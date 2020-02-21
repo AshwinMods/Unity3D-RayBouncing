2cb2430278d6ef0c57aac8845706b6a4f745c68e
AshwinMods <ashwinmods@gmail.com>
2020-02-21 19:26:49 +0530

    Initial Commit

---
+++ Assets/Scripts/Mirror.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour
+{
+	[SerializeField] float distMul = 2f;
+	[SerializeField] float speedMul = 2f;
+	[Space]
+	[SerializeField] float rotSpeed = 180f; //180* per sec
+
+	static bool autoMove = false; // was tired of draging each instance to UI Toggle
+	public bool AutoMove { 
+		set {
+			autoMove = value;
+		}
+	}
+	static bool autoRotate = false;
+	public bool AutoRotate
+	{
+		set
+		{
+			autoRotate = value;
+		}
+	}
+
+	Vector3 iPos;
+	Vector3 iDir;
+	private void OnEnable()
+	{
+		iPos = transform.position;
+		iDir = transform.right;
+	}
+	private void Update()
+	{
+		if (autoMove)
+		{
+			var mult = Mathf.Sin(Time.time * speedMul) * distMul;
+			transform.position = iPos + iDir * mult;
+		}
+		if (autoRotate)
+		{
+			transform.Rotate(Vector3.forward * rotSpeed * Time.deltaTime);
+		}
+	}
+}
---
+++ Assets/Scripts/RayReflector.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))] //Just in my case
+public class RayReflector : MonoBehaviour
+{
+	[SerializeField] LineRenderer lineRend;
+	[SerializeField] [Range(0.05f, 0.5f)] float rayWidth = 0.2f;
+	[SerializeField] int maxBounce = 6;
+	void Awake()
+	{
+		if (lineRend == null)
+			lineRend = GetComponent<LineRenderer>();
+		lineRend.startWidth = rayWidth;
+	}
+
+	//Almsot forgot you wanna do rotation :D
+	public void Set_Rotation(float z)
+	{
+		transform.eulerAngles = Vector3.forward * z;
+	}
+
+	public List<Vector3> RayPath2D(Vector2 origin, Vector2 dir)
+	{
+		List<Vector3> path = new List<Vector3>(); //V3? Thanks to line Renderer -_-!
+		path.Add(origin);
+
+		int count = 0;
+		RaycastHit2D hitInfo;
+		while (count < maxBounce)
+		{
+			hitInfo = Physics2D.Raycast(origin, dir);
+			if (!hitInfo) 
+				break;
+
+			dir = Vector2.Reflect(dir, hitInfo.normal);
+			origin = hitInfo.point + (dir * 0.001f); //To avoid Multiple Collision with same collider
+			path.Add(origin);
+			++count;
+		}
+		path.Add(origin + dir * 20F); //1E4F maybe, float.MaxValue or something that make sense
+		return path;
+	}
+
+	//just for the show of realtime, "RayPath2D" should be triggered when needed
+	public void LateUpdate()
+	{
+		var path = RayPath2D(transform.position, transform.up);
+		lineRend.positionCount = path.Count;
+		lineRend.SetPositions(path.ToArray());
+	}
+
+#if UNITY_EDITOR
+	[Header("Editor Test")]
+	[SerializeField] bool visualizeRay = false;
+	[SerializeField] bool continuous = false;
+	private void OnDrawGizmos() //was using OnValidate, but this works better for me
+	{
+		if (visualizeRay && lineRend != null)
+		{
+			if (!continuous)
+				visualizeRay = false;
+
+			lineRend.startWidth = rayWidth;
+			lineRend.endWidth = rayWidth;
+			LateUpdate();
+		}
+	}
+#endif
+}
